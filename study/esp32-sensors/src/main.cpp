0afbf486184710228f099455c0ebd85cdbcf567d
Carlo Miras <mirascarlo934@gmail.com>
2022-03-14 11:31:19 +0800

    feat: add MQ7 sensor support

--- study/esp32-sensors/src/main.cpp
+++ study/esp32-sensors/src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #include "DHT.h"
+#include "MQ7.h"
 
 #define DHT_PIN 33
 #define DHT_TYPE 22
 
-#define DHT_CYCLE 3000
+#define MQ7_PIN 32
+#define MQ7_VCC 5.0
+
+#define SENSOR_CYCLE 3000
 
 DHT dht(DHT_PIN, DHT_TYPE);
+MQ7 mq7(MQ7_PIN, MQ7_VCC);
 
 void setup() {
     Serial.begin(115200);
@@ -19,8 +24,9 @@ void loop() {
 
     Serial.println("");
 
-    delay(DHT_CYCLE);
+    delay(SENSOR_CYCLE);
 
+    float co = mq7.getPPM();
     float h = dht.readHumidity();       
     float t = dht.readTemperature();    // celsius
 
@@ -32,9 +38,10 @@ void loop() {
     // Compute heat index in Celsius (isFahreheit = false)
     float hic = dht.computeHeatIndex(t, h, false);
 
+    Serial.printf("CO: %f\n", co);
     Serial.printf("Humidity: %f\n", h);
     Serial.printf("Temp: %f\n", t);
     Serial.printf("Heat Index: %f\n", hic);
-    Serial.printf("Next reading in %u\n", DHT_CYCLE);
+    Serial.printf("Next reading in %u\n", SENSOR_CYCLE);
 
 }
