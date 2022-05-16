f2a49ec94ad937cc3ed9b57a4bb544d87b5e5410
Carlo Miras <mirascarlo934@gmail.com>
2022-05-16 17:52:34 +0800

    feat: converts 1 NETR CSV into better-formatted CSV

--- data-cruncher/main.py
+++ data-cruncher/main.py
@@ -1,32 +1,44 @@
 import csv
-import json
+import xlsxwriter
 
-# Get filename from user
-import json
-
-filename = input("Enter CSV: ")
+# Get details from user
+data_dir = input("Enter data directory: ")
+date = input("Enter data date: ")
+output_filename = input("Enter output CSV filename: ")
 
 # Process data
-with open(filename) as csv_file:
-    csvreader = csv.reader(csv_file, delimiter=';')
+ids = ["A", "B", "C", "D", "E"]
 
-    if "NETR" in filename:
+for id in ids:
+    with open(data_dir + "\\" + date + "-node-" + id + "-NETR") as csv_file:
 
         # Create new CSV with better-organized data points
-        rowNum = 1
-        for row in csvreader:
-            print(f"ROW {rowNum}")
-            packetIds = row[2]
-            packetIdsLen = len(packetIds)
-            packetIds = packetIds[1:packetIdsLen-1].split(",")
-            packetRtts = row[3]
-            packetRttsLen = len(packetRtts)
-            packetRtts = packetRtts[1:packetRttsLen-1].split(",")
-
-            print("PACKET IDs")
-            for packetId in packetIds:
-                print(packetId)
-
-            print("PACKET RTTs")
-            for packetRtt in packetRtts:
-                print(packetRtt)
+        csvreader = csv.reader(csv_file, delimiter=';')
+        newFilename = "C:\\Users\\user\\OneDrive\\UP\\2S 2021-2022\\CoE 198\\1_data\\processed" + "\\" + data_dir.split("\\")[-1]
+        with open(newFilename, mode='w') as newFile:
+            csvwriter = csv.writer(newFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            rowNum = 0
+            for row in csvreader:
+                print(row)
+                if rowNum == 0:
+                    csvwriter.writerow(["timestamp", "node ID", "sends", "bytes", "packet IDs", "packet RTTs"])
+                else:
+                    timestamp = row[0]
+                    bytes_ = row[1]
+                    nodeId = row[2]
+                    packetIds = row[3]
+                    packetRtts = row[4]
+                    sends = row[5]
+                    packetIdsLen = len(packetIds)
+                    packetIds = packetIds[1:packetIdsLen-1].split(",")
+                    packetRttsLen = len(packetRtts)
+                    packetRtts = packetRtts[1:packetRttsLen-1].split(",")
+                    for i in range(len(packetIds)):
+                        newTimestamp = timestamp
+                        if i < 10:
+                            newTimestamp += ":0" + str(i)
+                        else:
+                            newTimestamp += ":" + str(i)
+                        print(f"{newTimestamp},{nodeId},{sends},{bytes_},{packetIds[i]},{packetRtts[i]}")
+                        csvwriter.writerow([newTimestamp, nodeId, sends, bytes_, packetIds[i], packetRtts[i]])
+                rowNum = rowNum + 1
