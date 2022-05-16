99ae35e12b5e5f109d986f92206997cd0082a882
Carlo Miras <mirascarlo934@gmail.com>
2022-05-16 16:08:50 +0800

    feat: basic parsing

--- data-cruncher/main.py
+++ data-cruncher/main.py
@@ -1,16 +1,32 @@
 import csv
+import json
+
+# Get filename from user
+import json
 
 filename = input("Enter CSV: ")
 
-file = open(filename)
+# Process data
+with open(filename) as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=';')
 
-csvreader = csv.reader(file)
+    if "NETR" in filename:
 
-header = next(csvreader)
-print(header)
+        # Create new CSV with better-organized data points
+        rowNum = 1
+        for row in csvreader:
+            print(f"ROW {rowNum}")
+            packetIds = row[2]
+            packetIdsLen = len(packetIds)
+            packetIds = packetIds[1:packetIdsLen-1].split(",")
+            packetRtts = row[3]
+            packetRttsLen = len(packetRtts)
+            packetRtts = packetRtts[1:packetRttsLen-1].split(",")
 
-rows = []
-for row in csvreader:
-    rows.append(row)
-print(rows)
+            print("PACKET IDs")
+            for packetId in packetIds:
+                print(packetId)
 
+            print("PACKET RTTs")
+            for packetRtt in packetRtts:
+                print(packetRtt)
